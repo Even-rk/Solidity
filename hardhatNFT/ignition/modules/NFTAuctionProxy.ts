7b767fbf2f290f79405c2bfd8b86e45616849b56
Even <kwr011024@163.com>
2026-08-25 10:18:35 +0800

    feat(hardhatNFT/ignition): 添加NFTAuction可升级代理部署模块
    该模块用于部署可升级NFTAuction合约，包含部署实现合约、透明升级代理并完成初始化，同时获取代理管理员合约实例

---
+++ hardhatNFT/ignition/modules/NFTAuctionProxy.ts
@@ -0,0 +1,43 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const NFTAuctionProxyModule = buildModule("NFTAuctionProxyModule", (m) => {
+  // 获取代理合约管理员
+  const proxyAdmin = m.getAccount(0);
+
+  // 部署实现合约
+  const auctionImpl = m.contract("NFTAuction");
+
+  // 编码初始化函数调用
+  const encodedFunctionCall = m.encodeFunctionCall(auctionImpl, "initialize", [
+    proxyAdmin,
+  ]);
+
+  // 部署代理
+  const proxy = m.contract("TransparentUpgradeableProxy", [
+    auctionImpl,
+    proxyAdmin,
+    encodedFunctionCall,
+  ]);
+
+  // 读取代理合约管理员地址
+  const proxyAdminAddress = m.readEventArgument(
+    proxy,
+    "AdminChanged",
+    "newAdmin",
+  );
+
+  // 获取代理合约管理员合约
+  const proxyAdminContract = m.contractAt("ProxyAdmin", proxyAdminAddress);
+
+  return { proxyAdminContract, proxy };
+});
+
+const NFTAuctionModule = buildModule("NFTAuctionModule", (m) => {
+  const { proxy, proxyAdminContract } = m.useModule(NFTAuctionProxyModule);
+
+  const auction = m.contractAt("NFTAuction", proxy);
+
+  return { auction, proxy, proxyAdminContract };
+});
+
+export default NFTAuctionModule;
